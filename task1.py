4ab4f0a81fe1d5dd585bea3e243af477b978eff0
uvikmail <uvikmail@gmail.com>
2024-10-19 10:27:29 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+# 1st program
+print((9**0.5)*5)
+
+# 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+# 3rd programm
+print((2*2+2)==(2*(2+2)))
+
+# 4th programm
+print('123.456')
+print(123.456*10)
+print(int(1234.56)%10)
